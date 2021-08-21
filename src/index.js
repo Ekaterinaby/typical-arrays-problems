35fa4604c12044968815b8b88eaadb3bedd0d89c
ekaterinaby <katenalifee@gmail.com>
2021-08-21 18:29:36 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = array.length ? array[0] : 0;
 
-exports.min = function min (array) {
-  return 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array.length ? array[0] : 0;
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+
+        var total = 0;
+        for (var i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        var avg = total / array.length;
+        return avg;
+    }
 }
